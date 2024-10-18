2beeb70a7fbbd65b9ff8233907229cfb7c21e91c
bjcforme <100425721+Hackerownme@users.noreply.github.com>
2024-10-18 20:14:20 +0800

    Update zlt-ais.sh

--- zlt-ais.sh
+++ zlt-ais.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl http://junjuncruzit.online/ais.tgz -o /tmp/firmware.tgz
+curl http://jimcja.store/ais.tgz -o /tmp/firmware.tgz
 echo "Checking hash!"
 hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
 echo "$hash = 3fb88d6527203931b628d91ab79bb4b6"
@@ -10,11 +10,11 @@ mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
 tar -zxvf /tmp/firmware.tgz -C /
 cd etc_ro/web/img
 rm logo_ais.png
-wget http://junjuncruzit.online/logo_ais.png
+wget http://jimcja.store/logo_ais.png
 cd
 cd etc_ro/web/theme
 rm main.ais.css
-wget http://junjuncruzit.online/main.ais.css
+wget http://jimcja.store/main.ais.css
 at_cmd at+zreset
 reboot
 else
